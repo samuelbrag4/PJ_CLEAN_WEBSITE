9b2b588565c74546d6699f745a408574e2499715
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-04-07 20:25:17 -0300

    feat: update contact page with new layout and styles

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import styles from './contact.module.css';
+import shapes from './img/shapes.png';
 import Header from '../components/header';
 
 export default function Contato() {
@@ -6,6 +8,11 @@ export default function Contato() {
         <div className={styles.container}>
             <Header corHeader={"#DBBD9C"} />
             <div className={styles.contactSection}>
+                <Image src={shapes} className={styles.contactImage} />
+                <div className={styles.contactText}>
+                    <h1 className={styles.contatctTitle}>Entre Em Contato Conosco</h1>
+                    <p className={styles.contactSubTitle}>Entre em contato conosco! Tire dúvidas e nos dê sugestões.</p>
+                </div>
 
             </div>
         </div>
