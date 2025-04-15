38c89fbf72adfcd83064deda5d259bceea5dedbd
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-04-15 14:37:12 -0300

    feat: create component banner

--- src/app/skincare/page.jsx
+++ src/app/skincare/page.jsx
@@ -1,6 +1,7 @@
 import Footer from '../components/footer';
 import Header from '../components/header';
 import Hero from '../components/hero';
+import Banner from '../components/banner';
 
 export default function Skincare() {
     return (
@@ -14,6 +15,14 @@ export default function Skincare() {
                 textoPrimeiroBotao={"Dicas"}
                 corBotao={"#5FCED4"}
                 textoSegundoBotao={"Vídeos"}
+            />
+
+            <Banner 
+            image={"https://teiacosmeticos.com/cdn/shop/articles/skincare-para-estos-dias-en-casa-395165.jpg?v=1695166167"}
+            imageAlt={"Mulher aplicando creme na pele"}
+            title={"Transforme sua rotina de skincare em um ritual de autocuidado e bem-estar."}
+            description={"Sua pele é única e merece cuidados personalizados. Aqui, você encontra dicas, produtos e conselhos para criar um ritual de skincare personalizado que vai desde a limpeza diária até os tratamentos mais específicos. Saiba como produtos certos podem tratar suas preocupações específicas e transformar sua rotina em um momento para si em qualquer momento. Porque cuidar da pele é cuidar de você."}
+            corButton={"#5FCED4"}
             />
 
             <Footer />
