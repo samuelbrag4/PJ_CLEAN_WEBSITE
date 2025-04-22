69b33c9e9ee839e944133564a2f40bd8638aa845
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-04-22 08:36:22 -0300

    feat: add component Banner na página makeup

--- src/app/makeup/page.jsx
+++ src/app/makeup/page.jsx
@@ -1,6 +1,7 @@
 import Footer from '../components/footer';
 import Header from '../components/header';
 import Hero from '../components/hero';
+import Banner from '../components/banner'
 
 export default function Maquiagem() {
     return (
@@ -14,6 +15,14 @@ export default function Maquiagem() {
                 textoPrimeiroBotao={"Ler Mais"}
                 corBotao={"#F05080"}
                 textoSegundoBotao={"Sobre"}
+            />
+
+            <Banner
+                image={"https://negociosdebeleza.beautyfair.com.br/wp-content/uploads/2023/12/tendencias-de-maquiagem-tiktok.png"}
+                imageAlt={"Mulher aplicando maquiagem"}
+                title={"Descubra o poder transformador da maquiagem em sua rotina diária."}
+                description={"A maquiagem é uma forma de arte e expressão. Aqui, você encontra dicas, produtos e conselhos para criar looks incríveis, desde o básico até os mais elaborados. Aprenda como realçar sua beleza natural, experimentar novas tendências e transformar sua rotina de maquiagem em um momento de criatividade e autoconfiança. Porque maquiar-se é celebrar quem você é."}
+                corButton={"#F05080"}
             />
 
             <Footer />
