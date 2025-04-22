26ddc79dfad28fab153d557bff61d824b5cb5e68
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-04-22 11:13:58 -0300

    feat: adjusts spacing between cards and adds new cards to the accessories section

--- src/app/components/acessoriesSection/index.jsx
+++ src/app/components/acessoriesSection/index.jsx
@@ -25,6 +25,54 @@ const AcessoriesSection = () => {
               alt=""
             />
           </div>
+
+          <div className={styles.acessoriesCard}>
+            <div className={styles.acessoriesCardTexto}>
+              <h3 className={styles.acessoriesCardTitulo}>Massagem</h3>
+              <p className={styles.acessoriesCardParagrafo}>
+                O Corretivo Cobertura<br/> Extrema 24h Una<br/> proporciona alta<br/> cobertura
+                com efeito<br/> natural e longa duração,<br/> disfarçando olheiras,<br/>
+                manchas e imperfeições.
+              </p>
+            </div>
+
+            <img
+              src="https://glov.eu/cdn/shop/products/QuartzGuaSha_ef4b3c01-9fe4-4761-b21d-1097fe2307c6.png?v=1649078742"
+              alt=""
+            />
+          </div>
+
+          <div className={styles.acessoriesCard}>
+            <div className={styles.acessoriesCardTexto}>
+              <h3 className={styles.acessoriesCardTitulo}>Massagem</h3>
+              <p className={styles.acessoriesCardParagrafo}>
+                O Corretivo Cobertura<br/> Extrema 24h Una<br/> proporciona alta<br/> cobertura
+                com efeito<br/> natural e longa duração,<br/> disfarçando olheiras,<br/>
+                manchas e imperfeições.
+              </p>
+            </div>
+
+            <img
+              src="https://glov.eu/cdn/shop/products/QuartzGuaSha_ef4b3c01-9fe4-4761-b21d-1097fe2307c6.png?v=1649078742"
+              alt=""
+            />
+          </div>
+
+          <div className={styles.acessoriesCard}>
+            <div className={styles.acessoriesCardTexto}>
+              <h3 className={styles.acessoriesCardTitulo}>Massagem</h3>
+              <p className={styles.acessoriesCardParagrafo}>
+                O Corretivo Cobertura<br/> Extrema 24h Una<br/> proporciona alta<br/> cobertura
+                com efeito<br/> natural e longa duração,<br/> disfarçando olheiras,<br/>
+                manchas e imperfeições.
+              </p>
+            </div>
+
+            <img
+              src="https://glov.eu/cdn/shop/products/QuartzGuaSha_ef4b3c01-9fe4-4761-b21d-1097fe2307c6.png?v=1649078742"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </section>
